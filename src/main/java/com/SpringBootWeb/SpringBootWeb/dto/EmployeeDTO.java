f123313dbc9464b5affe01bcf6b77fcf88594e06
Sivapriyan7 <sivapriyan7siva@gmail.com>
2024-09-18 17:15:24 +0530

    ds

--- src/main/java/com/SpringBootWeb/SpringBootWeb/dto/EmployeeDTO.java
+++ src/main/java/com/SpringBootWeb/SpringBootWeb/dto/EmployeeDTO.java
@@ -21,77 +21,22 @@ public class EmployeeDTO {
     @Size(min = 3,max = 15, message = "Number of characters in name should be in range")
     private String name;
 
+    @NotNull(message = "Email of the employee cannot be blank")
     @Email(message = "Enter a valid Email")
     private String email;
 
+    @NotNull(message = "Age of the employee cannot be blank")
     @Max(value = 80, message = "Age can't be greater than 80")
     @Min(value = 18, message = "Age can't be lesser than 18")
     private Integer age;
 
+    @NotBlank(message = "Role of the Employee cannot be blank")
+    @Pattern(regexp = "^(ADMIN|USER)$",message = "Role of Employee can be USER or ADMIN")
+    private String role;
 
     private LocalDate dateOfJoining;
     @JsonProperty("isActive")
     private Boolean isActive;
 
-//    public EmployeeDTO()
-//    {
-//
-//    }
-//
-//    public EmployeeDTO(Long id, String name, String email, Integer age, LocalDate dateOfJoining, Boolean isActive) {
-//        this.id = id;
-//        this.name = name;
-//        this.email = email;
-//        this.age = age;
-//        this.dateOfJoining = dateOfJoining;
-//        this.isActive = isActive;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public Integer getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(Integer age) {
-//        this.age = age;
-//    }
-//
-//    public LocalDate getDateOfJoining() {
-//        return dateOfJoining;
-//    }
-//
-//    public void setDateOfJoining(LocalDate dateOfJoining) {
-//        this.dateOfJoining = dateOfJoining;
-//    }
-//
-//    public Boolean getActive() {
-//        return isActive;
-//    }
-//
-//    public void setActive(Boolean active) {
-//        isActive = active;
-//    }
+
 }
